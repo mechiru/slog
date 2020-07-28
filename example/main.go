6273ebe40e619d1d9a9d50610e61199fced247a1
mechiru <46036584+mechiru@users.noreply.github.com>
2020-07-28 10:21:42 +0900

    Fix example

--- example/main.go
+++ example/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	projectID := os.Getenv("PROJECT")
 
-	slog.Setup(projectID /*, slog.WithSeverity(slog.SeverityWarning)*/)
+	slog.Setup(projectID /*, slog.WithSeverity(slog.SeverityWarning), slog.WithLogLevel("warning")*/)
 
 	ctx := context.Background()
 	setupExporter(ctx, projectID)
@@ -25,28 +25,43 @@ func main() {
 	defer span.End()
 
 	slog.Debug("Debug/debug message")
+	slog.Debugf("Debugf/%s", "debug message")
 	slog.DebugWithSpan(span, "DebugWithSpan/debug message")
+	slog.DebugWithSpanf(span, "DebugWithSpanf/%s", "debug message")
 	slog.DebugWithCtx(ctx, "DebugWithCtx/debug message")
+	slog.DebugWithCtxf(ctx, "DebugWithCtxf/%s", "debug message")
 	sleep()
 
 	slog.Info("Info/info message")
+	slog.Infof("Infof/%s", "info message")
 	slog.InfoWithSpan(span, "InfoWithSpan/info message")
+	slog.InfoWithSpanf(span, "InfoWithSpanf/%s", "info message")
 	slog.InfoWithCtx(ctx, "InfoWithCtx/info message")
+	slog.InfoWithCtxf(ctx, "InfoWithCtxf/%s", "info message")
 	sleep()
 
 	slog.Warn("Warn/warn message")
+	slog.Warnf("Warnf/%s", "warn message")
 	slog.WarnWithSpan(span, "WarnWithSpan/warn message")
+	slog.WarnWithSpanf(span, "WarnWithSpanf/%s", "warn message")
 	slog.WarnWithCtx(ctx, "WarnWithCtx/warn message")
+	slog.WarnWithCtxf(ctx, "WarnWithCtx/%s", "warn message")
 	sleep()
 
 	slog.Error("Error/error message")
+	slog.Errorf("Errorf/%s", "error message")
 	slog.ErrorWithSpan(span, "ErrorWithSpan/error message")
+	slog.ErrorWithSpanf(span, "ErrorWithSpanf/%s", "error message")
 	slog.ErrorWithCtx(ctx, "ErrorWithCtx/error message")
+	slog.ErrorWithCtxf(ctx, "ErrorWithCtxf/%s", "error message")
 	sleep()
 
 	slog.ReportError("ReportError/error message")
+	slog.ReportErrorf("ReportErrorf/%s", "error message")
 	slog.ReportErrorWithSpan(span, "ErrorWithSpan/error message")
+	slog.ReportErrorWithSpanf(span, "ErrorWithSpanf/%s", "error message")
 	slog.ReportErrorWithCtx(ctx, "ErrorWithCtx/error message")
+	slog.ReportErrorWithCtxf(ctx, "ErrorWithCtxf/%s", "error message")
 	sleep()
 }
 
